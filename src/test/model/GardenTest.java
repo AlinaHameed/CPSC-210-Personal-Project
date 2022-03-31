32de6e754f8dde4d940c13626f5fbc4fea045f01
abh13 <abh13@students.cs.ubc.ca>
2022-03-31 10:05:05 -0700

    log genus test

--- src/test/model/GardenTest.java
+++ src/test/model/GardenTest.java
@@ -4,42 +4,54 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 
-import java.util.ArrayList;
-
 import static org.junit.jupiter.api.Assertions.*;
 
 public class GardenTest {
     private Garden testGarden;
     private Plant testPlant;
     private Plant testPlant2;
+    private Event allPlants;
+    private Event lAndAEvent;
+    private Event fAndAEvent;
+    private Event ficusEvent;
+    private Event lilyEvent;
+    private Event anthuriumEvent;
+    private Event emptyEvent;
 
     @BeforeEach
     void runBefore() {
         testGarden = new Garden("Alina's garden");
         testPlant = new Plant();
         testPlant2 = new Plant();
+        allPlants = new Event("garden checked to contain type: lily, ficus and anthurium");
+        lAndAEvent = new Event("garden checked to contain type: lily and anthurium");
+        fAndAEvent = new Event("garden checked to contain type: ficus and anthurium");
+        ficusEvent = new Event("garden checked to contain type: ficus");
+        lilyEvent = new Event("garden checked to contain type: lily");
+        anthuriumEvent = new Event("garden checked to contain type: anthurium");
+        emptyEvent = new Event("garden check to be empty");
     }
 
     @Test
     void testConstructor() {
-        assertEquals(0,testGarden.getNumPlants());
-        assertEquals("Alina's garden",testGarden.getName());
+        assertEquals(0, testGarden.getNumPlants());
+        assertEquals("Alina's garden", testGarden.getName());
     }
 
     @Test
     void testGetNumPlants() {
-        assertEquals(0,testGarden.getNumPlants());
+        assertEquals(0, testGarden.getNumPlants());
         testGarden.addPlantToGarden(testPlant);
-        assertEquals(1,testGarden.getNumPlants());
+        assertEquals(1, testGarden.getNumPlants());
     }
 
     @Test
     void testGetIndexPlant() {
         testGarden.addPlantToGarden(testPlant);
-        assertEquals(testPlant,testGarden.getIndexPlant(0));
+        assertEquals(testPlant, testGarden.getIndexPlant(0));
         testGarden.addPlantToGarden(testPlant2);
-        assertEquals(testPlant,testGarden.getIndexPlant(0));
-        assertEquals(testPlant2,testGarden.getIndexPlant(1));
+        assertEquals(testPlant, testGarden.getIndexPlant(0));
+        assertEquals(testPlant2, testGarden.getIndexPlant(1));
     }
 
     @Test
@@ -61,10 +73,10 @@ public class GardenTest {
     @Test
     void testAddingPlantToGarden() {
         testGarden.addPlantToGarden(testPlant);
-        assertEquals(testPlant,testGarden.getIndexPlant(0));
+        assertEquals(testPlant, testGarden.getIndexPlant(0));
         testGarden.addPlantToGarden(testPlant2);
-        assertEquals(testPlant,testGarden.getIndexPlant(0));
-        assertEquals(testPlant2,testGarden.getIndexPlant(1));
+        assertEquals(testPlant, testGarden.getIndexPlant(0));
+        assertEquals(testPlant2, testGarden.getIndexPlant(1));
     }
 
     @Test
@@ -74,9 +86,14 @@ public class GardenTest {
 
     @Test
     void testLogGenus() {
-
+        assertEquals("garden checked to contain type: lily, ficus and anthurium", allPlants.getDescription());
+        assertEquals("garden checked to contain type: lily and anthurium",lAndAEvent.getDescription());
+        assertEquals("garden checked to contain type: ficus and anthurium",fAndAEvent.getDescription());
+        assertEquals("garden checked to contain type: ficus",ficusEvent.getDescription());
+        assertEquals("garden checked to contain type: lily",lilyEvent.getDescription());
+        assertEquals("garden checked to contain type: anthurium",anthuriumEvent.getDescription());
+        assertEquals("garden check to be empty",emptyEvent.getDescription());
     }
-
 }
 
 
