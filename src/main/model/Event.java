bfa7321dcbe2ee5796a76787df1fc6327ccf31f2
abh13 <abh13@students.cs.ubc.ca>
2022-03-31 09:32:21 -0700

    event test

--- src/main/model/Event.java
+++ src/main/model/Event.java
@@ -38,27 +38,6 @@ public class Event {
     }
 
     @Override
-    public boolean equals(Object other) {
-        if (other == null) {
-            return false;
-        }
-        if (other.getClass() != this.getClass()) {
-            return false;
-        }
-
-        Event otherEvent = (Event) other;
-
-        return (this.dateLogged.equals(otherEvent.dateLogged)
-                &&
-                this.description.equals(otherEvent.description));
-    }
-
-//    @Override
-//    public int hashCode() {
-//        return (HASH_CONSTANT * dateLogged.hashCode() + description.hashCode());
-//    }
-
-    @Override
     public String toString() {
         return dateLogged.toString() + "\n" + description;
     }
